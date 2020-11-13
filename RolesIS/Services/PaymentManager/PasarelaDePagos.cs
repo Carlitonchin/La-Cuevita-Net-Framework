480f68c483a7aedc2f23eec43a3eba9439f747d6
ALabourdette <j.labourdette@estudiantes.matcom.uh.cu>
2020-11-12 16:57:00 -0800

    First Commit

---
+++ RolesIS/Services/PaymentManager/PasarelaDePagos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RolesIS.Utils
+{
+    public class PasarelaDePagos
+    {
+        public bool HacerTransferencia(string cuentaOrigen, string cuentaDestino, decimal monto)
+        {
+            var origenEnc = EncriptarCuenta(cuentaOrigen);
+            var destinoEnc = EncriptarCuenta(cuentaDestino);
+
+            var peticionSegura = CrearPeticionSegura(origenEnc, destinoEnc, monto);
+
+            var p = new ProcesadorDePagos();
+            p.CrearCanalSeguro();
+            
+            bool transaccionExitosa = p.RealizarPago(peticionSegura);
+            return transaccionExitosa;
+        }
+
+        private string EncriptarCuenta(string cuenta)
+        {
+            return cuenta;
+        }
+
+        private Object CrearPeticionSegura(string origenEncriptado, string destinoEncriptado, decimal monto)
+        {
+            return new Object();
+        }
+    }
+}
---
+++ RolesIS/Services/PaymentManager/ProcesadorDePagos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RolesIS.Utils
+{
+    public class ProcesadorDePagos
+    {
+        public ProcesadorDePagos()
+        {
+
+        }
+
+        public void CrearCanalSeguro()
+        {
+
+        }
+
+        public bool RealizarPago(Object peticion)
+        {
+            Random r = new Random();
+            int n = r.Next(5);
+            if (n == 0)
+                return false;
+            return true;
+        }
+    }
+}
